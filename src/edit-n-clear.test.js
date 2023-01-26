94058b69869ef020218940eda21f25599c8ad8cb
Jorge <jorge24abrahan@gmail.com>
2023-01-26 09:25:28 -0600

    Create mark-as-completed method tests

--- src/edit-n-clear.test.js
+++ src/edit-n-clear.test.js
@@ -1,6 +1,6 @@
 import ToDos from './modules/ToDos.js';
 
-describe('Tests to the edit and clear all completed methods', () => {
+describe('Tests to the edit, check and clear all completed methods', () => {
   // Arrange
   document.body.innerHTML = `
   <ul id="cntTodos"></ul>
@@ -13,11 +13,20 @@ describe('Tests to the edit and clear all completed methods', () => {
   const todoId = todo.todos[0].id;
   const todoHTML = cntTodos.querySelector(`#${todoId}`);
   const todoInput = todoHTML.querySelector('input[type="text"]');
+  const todoCheck = todoHTML.querySelector('input[type="checkbox"]');
   it('Should edit the description of the created todo from "one" to "two"', () => {
     // Assert
     expect(todo.todos[0].description).toBe('one');
     todoInput.value = 'two';
     todo.focusOut(todoHTML, todoInput);
     expect(todo.todos[0].description).toBe('two');
+  });
+
+  it('Should change the completed status of the todo from "false" to "true"', () => {
+    // Act
+    todoCheck.checked = true;
+    todo.check(todoHTML, todoCheck);
+    // Assert
+    expect(todo.todos[0].completed).toBeTruthy();
   });
 });
